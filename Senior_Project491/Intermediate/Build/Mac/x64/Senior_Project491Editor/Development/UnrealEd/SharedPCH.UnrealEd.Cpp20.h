7871e02042dfaae64637071941bfc67bdf8fd626
Isaac Sanchez <25628340+Isaacsan02@users.noreply.github.com>
2025-03-05 19:47:04 -0800

    Starting point and third person added

---
+++ Senior_Project491/Intermediate/Build/Mac/x64/Senior_Project491Editor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
+#include "/Users/marinawild/Documents/GitHub/cpsc-491-kevin-running-doubledash-game/Senior_Project491/Intermediate/Build/Mac/x64/Senior_Project491Editor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
+#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
